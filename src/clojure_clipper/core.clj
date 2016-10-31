15b805478929aa22d3984c9e9df14430370956cc
Raheel Ahmad <ahmadraheel@gmail.com>
2016-10-30 21:37:46 -0700

    Write proper tests for parsing

--- src/clojure_clipper/core.clj
+++ src/clojure_clipper/core.clj
@@ -1,15 +1,8 @@
 (ns clojure-clipper.core
   (:require [net.cgrand.enlive-html :as html]
             [clojure.string :as str]
-            clojure-clipper.properties))
-
-;; TODO
-;; - write tests
-
-(def sources
-  [
-   {:symbol :nyt :name "NYT" :url  "htmls/nyt.html"}
-   {:symbol :alr :name "All Recipes" :url  "htmls/allrecipes.html"}]) 
+            [clojure.pprint :as pp]
+            [clojure-clipper.properties :refer :all]))
 
 (defn fetch [url] (html/html-resource url))
 
@@ -22,27 +15,30 @@
   (let [selectors (:selectors prop)
         source-symbol (:symbol source)
         ;; selector is found either in :selector,
-        ;; or for in :selectors map with key as source's symbol
+        ;; or in the :selectors map with key as source's symbol
         selector (or (:selector prop)
                      (get selectors source-symbol))
         key (:key prop)
         name (:title prop)
         prop-container (get-prop-container content key)
-        prop-value (selector prop-container)]
-    (if (= key "recipeInstructions")
-      (clojure.pprint/pprint prop-container))
-    {name prop-value})) 
+        prop-value (selector prop-container)
+        prop-value-str (apply str prop-value)
+        trimmed-prop-value (-> prop-value-str str/trim str/trim-newline)
+        ]
+    ;; (if (= key "recipeInstructions")
+    ;;   (clojure.pprint/pprint prop-container))
+    {name trimmed-prop-value}))
 
 (defn parse-recipe [source]
-  (let [url (:url source)
-        page (fetch url)]
-    ; build the result as (this is just one possibility):
-    {(:name  source) ; 1. the source name
-     (apply merge    ; 3. merge properties into a single map
-            (map #(get-prop source page %) properties) ; 2. get parsed values for each property; produces a list of maps
-            )}))
+  (let [page
+        (html/html-resource (:url source))]
+                                                        ; build the result as (this is just one possibility):
+    {
+     (:name  source)                                    ; 1. the source name
+     (apply merge                                       ; 3. merge properties into a single map
+            (map #(get-prop source page %) properties)) ; 2. get parsed values for each property ; produces a list of maps
+     }))
 
 
-(clojure.pprint/pprint
-(map parse-recipe sources)
-)
+;; (clojure.pprint/pprint
+;;  (map parse-recipe sources))
--- test/clojure_clipper/core_test.clj
+++ test/clojure_clipper/core_test.clj
@@ -1,7 +1,34 @@
 (ns clojure-clipper.core-test
   (:require [clojure.test :refer :all]
-            [clojure-clipper.core :refer :all]))
+            [clojure-clipper.core :as clipper]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def sources
+  [
+   {:symbol :nyt :name "NYT" :url  "htmls/nyt.html"}
+   {:symbol :alr :name "All Recipes" :url  "htmls/allrecipes.html"}
+   ])
+
+(def nyt-fixture (first (filter #(= (:symbol %) :nyt) sources)))
+(def alr-fixture (first (filter #(= (:symbol %) :alr) sources)))
+
+(deftest recipe-testing
+  (testing "Recipe parsing"
+    (testing "NYT"
+      (let [nyt-parsed (get (clipper/parse-recipe nyt-fixture) "NYT")
+            name (get nyt-parsed "Name")
+            description (get nyt-parsed "Description")
+            image (get nyt-parsed "Photo")]
+        (is (= name "Lemon and Garlic Chicken With Mushrooms"))
+        (is (= image "https://static01.nyt.com/images/2014/03/22/science/28recipehealth/28recipehealth-articleLarge.jpg"))
+        (is (= description "In this Provençal rendition of pan-cooked chicken breasts, the mushrooms take on and added dimension of flavor as they deglaze the pan with the help of one of their favorite partners, dry white wine."))
+        ))
+    (testing "All Recipes"
+      (let [all-recipe-parsed (get (clipper/parse-recipe alr-fixture) "All Recipes")
+            name (get all-recipe-parsed "Name")
+            description (get all-recipe-parsed "Description")
+            image (get all-recipe-parsed "Photo")]
+        (is (= name "Chef John's Ricotta Meatballs"))
+        (is (= image "http://images.media-allrecipes.com/userphotos/560x315/1077229.jpg"))
+        (is (= description "See how to turn regular beef meatballs into a ricotta-filled sensation."))
+        ))
+    ))
